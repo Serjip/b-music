6732ffb84d3ab93a01358ba22a6883cca0981bf1
Serji <Serji.popov@gmail.com>
2013-10-01 17:07:24 +0400

    Add table and two custom views

---
+++ b-music/AppController.h
@@ -0,0 +1,13 @@
+//
+//  AppController.h
+//  b-music
+//
+//  Created by Sergey P on 01.10.13.
+//  Copyright (c) 2013 Sergey P. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppController : NSObject
+
+@end
---
+++ b-music/AppController.m
@@ -0,0 +1,13 @@
+//
+//  AppController.m
+//  b-music
+//
+//  Created by Sergey P on 01.10.13.
+//  Copyright (c) 2013 Sergey P. All rights reserved.
+//
+
+#import "AppController.h"
+
+@implementation AppController
+
+@end
