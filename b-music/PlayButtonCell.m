3567f9ea206d098c9cd25b683ee4dce6c4094a84
Serji <Serji.popov@gmail.com>
2013-10-07 15:05:20 +0400

    Fix play button cell

--- b-music/PlayButtonCell.m
+++ b-music/PlayButtonCell.m
@@ -7,20 +7,17 @@
 //
 
 #import "PlayButtonCell.h"
-@implementation PlayButtonCell{
-    NSTrackingArea * trackingArea;
-}
+
+#define thikness 1
+#define h 44
+#define w 44
+
+#define p 17
+
+@implementation PlayButtonCell
 
 - (void)drawRect:(NSRect)dirtyRect
 {
-    double x1=(self.frame.size.width+self.frame.size.height)/2-1;
-    double y1=x1;
-    double x0=self.frame.origin.x;
-    double y0=self.frame.origin.y;
-    
-    double t=1;//Thikness
-    double p=16;//Padding
-    
     CGContextRef ctx = [[NSGraphicsContext currentContext] graphicsPort];
     
     if ([self.cell isHighlighted]) {
@@ -31,18 +28,20 @@
         CGContextSetRGBFillColor (ctx, 150/255.0, 150/255.0, 150/255.0, 1);
     }
     
-    CGContextSetAlpha(ctx, 0.5);
-    
-    CGContextSetLineWidth(ctx, t);
-    CGContextStrokeEllipseInRect(ctx, CGRectMake(t/2, t/2, x1-t, y1-t));
+    CGContextSetLineWidth(ctx, thikness);
+    CGContextStrokeEllipseInRect(ctx, CGRectMake(thikness, thikness, w-2*thikness, h-2*thikness));
     
     if(_pause){
-        CGContextFillRect (ctx, CGRectMake (x0+p, y0+p, (x1-p*2)/3, y1-p*2 ));
-        CGContextFillRect (ctx, CGRectMake (x0+p+(x1-p*2)*2/3, y0+p, (x1-p*2)/3, y1-p*2 ));
+        CGContextSetLineWidth(ctx, thikness*2);
+        CGContextMoveToPoint(ctx, w/2-3, p);
+        CGContextAddLineToPoint(ctx, w/2-3 , h-p);
+        CGContextMoveToPoint(ctx, w/2+3, p);
+        CGContextAddLineToPoint(ctx, w/2+3 , h-p);
+        CGContextStrokePath(ctx);
     }else{
-        CGContextMoveToPoint(ctx, x0+p, y0+p);
-        CGContextAddLineToPoint(ctx, x0+p , y1-p);
-        CGContextAddLineToPoint(ctx, x1-p/1.1 , y1/2);
+        CGContextMoveToPoint(ctx, p, p);
+        CGContextAddLineToPoint(ctx, p , h-p);
+        CGContextAddLineToPoint(ctx, w-p/1.1 , h/2);
         CGContextClosePath(ctx);
     }
     
@@ -52,29 +51,6 @@
 -(void) setPauseState:(BOOL)state{
     _pause=state;
     [self setNeedsDisplay:YES];
-}
-
--(void)mouseEntered:(NSEvent *)theEvent {
-    [[self animator] setAlphaValue:1.0];
-    [self setNeedsDisplay:YES];
-}
-
--(void)mouseExited:(NSEvent *)theEvent
-{
-    [[self animator] setAlphaValue:0.5];
-    [self setNeedsDisplay:YES];
-}
-
--(void)updateTrackingAreas
-{
-    if(trackingArea != nil) {
-        [self removeTrackingArea:trackingArea];
-    }
-    trackingArea = [ [NSTrackingArea alloc] initWithRect:[self bounds]
-                                                 options:(NSTrackingMouseEnteredAndExited | NSTrackingActiveAlways)
-                                                   owner:self
-                                                userInfo:nil];
-    [self addTrackingArea:trackingArea];
 }
 
 @end
