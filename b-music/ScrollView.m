ffb1242eccea13331e4990abe6fefee08740986f
Serji <Serji.popov@gmail.com>
2013-10-28 21:03:23 +0400

    Before layer checing

---
+++ b-music/ScrollView.m
@@ -0,0 +1,29 @@
+//
+//  ScrollView.m
+//  b-music
+//
+//  Created by Sergey P on 28.10.13.
+//  Copyright (c) 2013 Sergey P. All rights reserved.
+//
+
+#import "ScrollView.h"
+
+@implementation ScrollView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	[super drawRect:dirtyRect];
+	
+    // Drawing code here.
+}
+
+@end
